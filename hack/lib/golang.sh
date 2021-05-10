58f801a0d640b32e5950cfd2e304111356d95fce
NeverMore <842125706@qq.com>
2021-05-10 18:00:11 +0800

    read PRERELEASE_LIFECYCLE_GEN

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -69,6 +69,7 @@ readonly KUBE_SUPPORTED_TEST_PLATFORMS=(
 
 # The set of server targets that we are only building for Linux
 # If you update this list, please also update build/BUILD.
+# server 端所有的 targets，最后按空格为分隔符，打印出所有的targets
 kube::golang::server_targets() {
   local targets=(
     cmd/kube-proxy
@@ -83,9 +84,14 @@ kube::golang::server_targets() {
   )
   echo "${targets[@]}"
 }
-
+# IFS 指定分隔符，read 指定 变量类型列表，<<< 直接传字符串 按空格解构成列表
+# <<< 一般传递字符串，<< 一般用于追加文件，< 用于写入新文件
 IFS=" " read -ra KUBE_SERVER_TARGETS <<< "$(kube::golang::server_targets)"
 readonly KUBE_SERVER_TARGETS
+# ${KUBE_SERVER_TARGETS[@]##*/} 将 KUBE_SERVER_TARGETS 列表的所有元素进行匹配，将所有满足 末尾是 / 的元素删除
+# ${变量名##匹配规则} 从变量开头进行规则匹配，将符合数据最长的数据删除（贪婪匹配）
+# KUBE_SERVER_BINARIES 表示 server 端需要编译的所有 二进制文件
+# Server 端不支持 windows 操作系统
 readonly KUBE_SERVER_BINARIES=("${KUBE_SERVER_TARGETS[@]##*/}")
 
 # The set of server targets we build docker images for
@@ -253,15 +259,19 @@ kube::golang::setup_platforms
 
 # The set of client targets that we are building for all platforms
 # If you update this list, please also update build/BUILD.
+# client 端所有的 targets
 readonly KUBE_CLIENT_TARGETS=(
   cmd/kubectl
   cmd/kubectl-convert
 )
+# 同样 KUBE_CLIENT_BINARIES 表示 所有 client 端的 需要编译的二进制文件
+# KUBE_CLIENT_BINARIES_WIN 表示 windows 版的 exe 文件
 readonly KUBE_CLIENT_BINARIES=("${KUBE_CLIENT_TARGETS[@]##*/}")
 readonly KUBE_CLIENT_BINARIES_WIN=("${KUBE_CLIENT_BINARIES[@]/%/.exe}")
 
 # The set of test targets that we are building for all platforms
 # If you update this list, please also update build/BUILD.
+# 客户端 有关测试 test 的所有 targets，最后按空格为分隔符，打印出所有的targets
 kube::golang::test_targets() {
   local targets=(
     cmd/gendocs
@@ -276,6 +286,8 @@ kube::golang::test_targets() {
   )
   echo "${targets[@]}"
 }
+# 同样 将所有的 测试 targets 传入 列表 KUBE_TEST_TARGETS
+# 拿到 linux 版 KUBE_TEST_BINARIES 二进制文件，和 windows 版 KUBE_TEST_BINARIES_WIN exe 文件
 IFS=" " read -ra KUBE_TEST_TARGETS <<< "$(kube::golang::test_targets)"
 readonly KUBE_TEST_TARGETS
 readonly KUBE_TEST_BINARIES=("${KUBE_TEST_TARGETS[@]##*/}")
@@ -294,6 +306,7 @@ readonly KUBE_TEST_PORTABLE=(
 # need to target server platforms.
 # These binaries will be distributed in the kubernetes-test tarball.
 # If you update this list, please also update build/BUILD.
+# 服务端，可以直接跑在 k8s 上的 二进制测试工具
 kube::golang::server_test_targets() {
   local targets=(
     cmd/kubemark
@@ -306,7 +319,7 @@ kube::golang::server_test_targets() {
 
   echo "${targets[@]}"
 }
-
+# 操作同上
 IFS=" " read -ra KUBE_TEST_SERVER_TARGETS <<< "$(kube::golang::server_test_targets)"
 readonly KUBE_TEST_SERVER_TARGETS
 readonly KUBE_TEST_SERVER_BINARIES=("${KUBE_TEST_SERVER_TARGETS[@]##*/}")
@@ -317,6 +330,7 @@ readonly KUBE_TEST_SERVER_PLATFORMS=("${KUBE_SERVER_PLATFORMS[@]:+"${KUBE_SERVER
 # memory of 15GiB.
 # This variable can be overwritten at your own risk.
 # It's defaulting to 20G to provide some headroom.
+# 如果需要开启 并行模式编译，就需要 内存大于 20G
 readonly KUBE_PARALLEL_BUILD_MEMORY=${KUBE_PARALLEL_BUILD_MEMORY:-20}
 
 readonly KUBE_ALL_TARGETS=(
@@ -348,6 +362,7 @@ readonly KUBE_COVERAGE_INSTRUMENTED_PACKAGES=(
 # KUBE_CGO_OVERRIDES is a space-separated list of binaries which should be built
 # with CGO enabled, assuming CGO is supported on the target platform.
 # This overrides any entry in KUBE_STATIC_LIBRARIES.
+#  使用 cgo
 IFS=" " read -ra KUBE_CGO_OVERRIDES_LIST <<< "${KUBE_CGO_OVERRIDES:-}"
 readonly KUBE_CGO_OVERRIDES_LIST
 # KUBE_STATIC_OVERRIDES is a space-separated list of binaries which should be
@@ -356,11 +371,14 @@ readonly KUBE_CGO_OVERRIDES_LIST
 IFS=" " read -ra KUBE_STATIC_OVERRIDES_LIST <<< "${KUBE_STATIC_OVERRIDES:-}"
 readonly KUBE_STATIC_OVERRIDES_LIST
 
+# 判断是否是 静态链接库
 kube::golang::is_statically_linked_library() {
   local e
   # Explicitly enable cgo when building kubectl for darwin from darwin.
+  # 如果是 darwin 系统，并且 target 是 kubectl 那么就是 静态链接库，返回1
   [[ "$(go env GOHOSTOS)" == "darwin" && "$(go env GOOS)" == "darwin" &&
     "$1" == *"/kubectl" ]] && return 1
+  # 后面代码段是判断，如果 target 有在 cgo 的列表中的，就是 静态链接库，返回1
   if [[ -n "${KUBE_CGO_OVERRIDES_LIST:+x}" ]]; then
     for e in "${KUBE_CGO_OVERRIDES_LIST[@]}"; do [[ "${1}" == *"/${e}" ]] && return 1; done;
   fi
@@ -373,15 +391,20 @@ kube::golang::is_statically_linked_library() {
 
 # kube::binaries_from_targets take a list of build targets and return the
 # full go package to be built
+#  根据之前设置的 targets 列表，返回 完整的 带有域名的 package 名称
 kube::golang::binaries_from_targets() {
   local target
   for target; do
     # If the target starts with what looks like a domain name, assume it has a
     # fully-qualified package name rather than one that needs the Kubernetes
     # package prepended.
+    # 如果 是以 域名 开头，那就不需要在添加 kubernetes 域名
+    # [[:alnum:]]		匹配任意字母数字字符0-9，A-Z，a-z
+    # 也就是 匹配到 xxx.xxx/ 这种域名格式，就不需要添加
     if [[ "${target}" =~ ^([[:alnum:]]+".")+[[:alnum:]]+"/" ]]; then
       echo "${target}"
     else
+      # KUBE_GO_PACKAGE=k8s.io/kubernetes 也就是添加 k8s 的域名
       echo "${KUBE_GO_PACKAGE}/${target}"
     fi
   done
@@ -395,7 +418,9 @@ kube::golang::host_platform() {
 
 # Takes the platform name ($1) and sets the appropriate golang env variables
 # for that platform.
+# 获取平台名称 ($1)，并为该平台设置适当的 golang env变量。
 kube::golang::set_platform_envs() {
+  # 如果传入的参数 平台名称 为空，直接报错
   [[ -n ${1-} ]] || {
     kube::log::error_exit "!!! Internal error. No platform set in kube::golang::set_platform_envs"
   }
@@ -404,6 +429,7 @@ kube::golang::set_platform_envs() {
   export GOARCH=${platform##*/}
 
   # Do not set CC when building natively on a platform, only if cross-compiling
+  # 如果 传入的 platform 不是 本机的系统，设置交叉编译 CC 的变量，本机不设置
   if [[ $(kube::golang::host_platform) != "$platform" ]]; then
     # Dynamic CGO linking for other server architectures than host architecture goes here
     # If you want to include support for more server platforms than these, add arch-specific gcc names here
@@ -432,6 +458,7 @@ kube::golang::set_platform_envs() {
   fi
 
   # if CC is defined for platform then always enable it
+  # 如果交叉编译 允许，那么就开启
   ccenv=$(echo "$platform" | awk -F/ '{print "KUBE_" toupper($1) "_" toupper($2) "_CC"}')
   if [ -n "${!ccenv-}" ]; then 
     export CGO_ENABLED=1
@@ -568,9 +595,11 @@ kube::golang::place_bins() {
 
 # Try and replicate the native binary placement of go install without
 # calling go install.
+# 返回 output 的输出目录
 kube::golang::outfile_for_binary() {
   local binary=$1
   local platform=$2
+  # output_path = _output/local/bin
   local output_path="${KUBE_GOPATH}/bin"
   local bin
   bin=$(basename "${binary}")
@@ -580,6 +609,8 @@ kube::golang::outfile_for_binary() {
   if [[ ${GOOS} == "windows" ]]; then
     bin="${bin}.exe"
   fi
+  # 最终返回的就是 _output/local/bin/platform/path
+  # 例如 我本机的就是 _output/local/bin/linux/amd64
   echo "${output_path}/${bin}"
 }
 
@@ -680,8 +711,10 @@ kube::golang::build_some_binaries() {
    fi
 }
 
+# 根据不同的 platform 来编译二进制文件
 kube::golang::build_binaries_for_platform() {
   # This is for sanity.  Without it, user umasks can leak through.
+  # umask 0022 表示默认创建新文件权限为755 也就是 rxwr-xr-x
   umask 0022
 
   local platform=$1
@@ -692,7 +725,14 @@ kube::golang::build_binaries_for_platform() {
 
   V=2 kube::log::info "Env for ${platform}: GOOS=${GOOS-} GOARCH=${GOARCH-} GOROOT=${GOROOT-} CGO_ENABLED=${CGO_ENABLED-} CC=${CC-}"
 
+  # 遍历每一个 target 进行编译
+  # 下面这个遍历时进行归类：
+  # 1. .test 结尾：放入 tests 列表
+  # 2. 静态链接库：放入 statics 列表
+  # 3. 非静态链接库：放入 nonstatics 列表
   for binary in "${binaries[@]}"; do
+    # 如果是以.test 结尾，只有两个 ： e2e.test 和  e2e_node.test
+    # =~ 表示允许使用正则
     if [[ "${binary}" =~ ".test"$ ]]; then
       tests+=("${binary}")
     elif kube::golang::is_statically_linked_library "${binary}"; then
@@ -703,7 +743,9 @@ kube::golang::build_binaries_for_platform() {
   done
 
   local -a build_args
+  # 编译 静态链接库
   if [[ "${#statics[@]}" != 0 ]]; then
+    # 编译的参数，这里的参数，在 build_binaries 前面都已经设置过了
     build_args=(
       -installsuffix static
       ${goflags:+"${goflags[@]}"}
@@ -712,9 +754,11 @@ kube::golang::build_binaries_for_platform() {
       -ldflags "${goldflags:-}"
       -tags "${gotags:-}"
     )
+    # 开始执行编译函数 build_some_binaries
     CGO_ENABLED=0 kube::golang::build_some_binaries "${statics[@]}"
   fi
 
+  # 编译 非静态链接库 ，与静态链接库编译的区别就是，是否启用 cgo
   if [[ "${#nonstatics[@]}" != 0 ]]; then
     build_args=(
       ${goflags:+"${goflags[@]}"}
@@ -726,8 +770,10 @@ kube::golang::build_binaries_for_platform() {
     kube::golang::build_some_binaries "${nonstatics[@]}"
   fi
 
+  # 编译.test ，直接用 go test 编译，并且执行测试文件
   for test in "${tests[@]:+${tests[@]}}"; do
     local outfile testpkg
+    # 输出目录
     outfile=$(kube::golang::outfile_for_binary "${test}" "${platform}")
     testpkg=$(dirname "${test}")
 
@@ -744,10 +790,16 @@ kube::golang::build_binaries_for_platform() {
 }
 
 # Return approximate physical memory available in gigabytes.
+# 返回可以用的 物理内存
+# 有三种方式：
+# 1. /proc/meminfo 文件中的 MemAvailable 字段
+# 2. /proc/meminfo 文件中的 MemTotal 字段
+# 3. 如果是 docker 容器， sysctl -n hw.memsize 2>/dev/null
 kube::golang::get_physmem() {
   local mem
 
   # Linux kernel version >=3.14, in kb
+  # 查看 /proc/meminfo 文件中的 MemAvailable 字段，查看还有多少可用的内存
   if mem=$(grep MemAvailable /proc/meminfo | awk '{ print $2 }'); then
     echo $(( mem / 1048576 ))
     return
@@ -763,12 +815,14 @@ kube::golang::get_physmem() {
   # run in a Linux container (because it's only used in the multiple
   # platform case, which is a Dockerized build), but this is provided
   # for completeness.
+  # 如果是 docker 容器，执行此命令查看
   if mem=$(sysctl -n hw.memsize 2>/dev/null); then
     echo $(( mem / 1073741824 ))
     return
   fi
 
   # If we can't infer it, just give up and assume a low memory system
+  # 如果都没有，那么 返回1，假设内存不足
   echo 1
 }
 
@@ -779,32 +833,47 @@ kube::golang::get_physmem() {
 #     are built.
 #   KUBE_BUILD_PLATFORMS - Incoming variable of targets to build for.  If unset
 #     then just the host architecture is built.
+# build_binaries 函数用于编译出 二进制文件
+# 输入：targets and go flags 如果targets为空，表示编译所有
+# KUBE_BUILD_PLATFORMS 环境变量：使用什么平台进行构建，默认是主机模式
 kube::golang::build_binaries() {
   # Create a sub-shell so that we don't pollute the outer environment
+  # 这里使用 () 来启动一个 sub-shell 来执行下面代码，以至于不会污染外部的环境变量
   (
     # Check for `go` binary and set ${GOPATH}.
+    # setup_env 函数用于构建一个 可以使用的 Go 执行环境
     kube::golang::setup_env
+    # 打印 info 消息
     V=2 kube::log::info "Go version: $(GOFLAGS='' go version)"
 
+    # 拿到 机器 的版本信息 linux/amd64
     local host_platform
     host_platform=$(kube::golang::host_platform)
 
+    # 设置 局部变量
     local goflags goldflags goasmflags gogcflags gotags
     # If GOLDFLAGS is unset, then set it to the a default of "-s -w".
     # Disable SC2153 for this, as it will throw a warning that the local
     # variable goldflags will exist, and it suggest changing it to this.
     # shellcheck disable=SC2153
+
+    # 设置 编译时的标记选项
+    # goldflags -s -w 选项来禁用符号表以及debug信息
+    # kube::version::ldflags 函数 打印需要传递给 go build 的 -ldflags 参数的值
     goldflags="${GOLDFLAGS=-s -w -buildid=} $(kube::version::ldflags)"
     goasmflags="-trimpath=${KUBE_ROOT}"
     gogcflags="${GOGCFLAGS:-} -trimpath=${KUBE_ROOT}"
 
     # extract tags if any specified in GOFLAGS
     # shellcheck disable=SC2001
+    # 提取 在 GOFLAGS 中设置的 tag
     gotags="selinux,notest,$(echo "${GOFLAGS:-}" | sed -e 's|.*-tags=\([^-]*\).*|\1|')"
 
+    # targets 是一个列表
     local -a targets=()
     local arg
 
+    # 将传入的参数做判断，如果 以 - 开头，表示是 go flags，其他为 targets
     for arg; do
       if [[ "${arg}" == -* ]]; then
         # Assume arguments starting with a dash are flags to pass to go.
@@ -814,24 +883,36 @@ kube::golang::build_binaries() {
       fi
     done
 
+    # ${#targets[@]} 表示数组的元素个数
+    # 如果传入的 targets 为空，则直接使用所有的 targets
     if [[ ${#targets[@]} -eq 0 ]]; then
       targets=("${KUBE_ALL_TARGETS[@]}")
     fi
 
+
     local -a platforms
+    # 读取 KUBE_BUILD_PLATFORMS  本机的系统版本信息
     IFS=" " read -ra platforms <<< "${KUBE_BUILD_PLATFORMS:-}"
+    # 如果 没有 配置 KUBE_BUILD_PLATFORMS ，则直接 获取 本机的 系统信息
     if [[ ${#platforms[@]} -eq 0 ]]; then
       platforms=("${host_platform}")
     fi
 
     local -a binaries
+    # read -r 屏蔽转义字符\
+    # kube::golang::binaries_from_targets 传入了 所有 targets 的元素
+    # binaries 就是 包括域名的完整的 package 名称
     while IFS="" read -r binary; do binaries+=("$binary"); done < <(kube::golang::binaries_from_targets "${targets[@]}")
 
     local parallel=false
+    # 如果需要编译的平台 platforms 只有一个
     if [[ ${#platforms[@]} -gt 1 ]]; then
       local gigs
+      # 获取 物理机的可用内存
       gigs=$(kube::golang::get_physmem)
 
+      # 这里判断是否可以开启 并行模式 编译，要求：可用内存大于等于20G
+      # KUBE_PARALLEL_BUILD_MEMORY 默认是 20
       if [[ ${gigs} -ge ${KUBE_PARALLEL_BUILD_MEMORY} ]]; then
         kube::log::status "Multiple platforms requested and available ${gigs}G >= threshold ${KUBE_PARALLEL_BUILD_MEMORY}G, building platforms in parallel"
         parallel=true
@@ -841,12 +922,16 @@ kube::golang::build_binaries() {
       fi
     fi
 
+    # 如果开启 并行模式
     if [[ "${parallel}" == "true" ]]; then
       kube::log::status "Building go targets for {${platforms[*]}} in parallel (output will appear in a burst when complete):" "${targets[@]}"
       local platform
+      # 如果有多个 编译的平台，我这里就只有一个本机
       for platform in "${platforms[@]}"; do (
+          # set_platform_envs 函数 根据不同的 platform 设置不同的 golang 环境
           kube::golang::set_platform_envs "${platform}"
           kube::log::status "${platform}: build started"
+
           kube::golang::build_binaries_for_platform "${platform}"
           kube::log::status "${platform}: build finished"
         ) &> "/tmp//${platform//\//_}.build" &
@@ -862,6 +947,7 @@ kube::golang::build_binaries() {
       done
 
       exit "${fails}"
+    # 如果不开启 并行模式
     else
       for platform in "${platforms[@]}"; do
         kube::log::status "Building go targets for ${platform}:" "${targets[@]}"
--- hack/make-rules/build.sh
+++ hack/make-rules/build.sh
@@ -20,9 +20,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+#  KUBE_ROOT 变量为：hack/make-rules/../.. 找到 根目录
 KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/../..
 KUBE_VERBOSE="${KUBE_VERBOSE:-1}"
+# 载入 init.sh 脚本，功能：
+# 1. 初始化一些常量，并将其 export 到环境变量中
+# 2. 加载了一些 工具函数
+# 3. 捕捉 ERR 信号，并且 确保 bash 的版本大于4
 source "${KUBE_ROOT}/hack/lib/init.sh"
 
+# 调用函数 kube::golang::build_binaries 并且传入 ./vendor/k8s.io/code-generator/cmd/prerelease-lifecycle-gen 参数
+#
 kube::golang::build_binaries "$@"
 kube::golang::place_bins
